4a656045c7bdd8914d2891523a5e3174f035f63b
Mehmet Çanak <mehmet.canak.36@gmail.com>
2021-09-25 21:55:55 +0300

    sanctum add

--- REST-API-With-Sanctum-Authentication/app/Models/Sanctum/User.php
+++ REST-API-With-Sanctum-Authentication/app/Models/Sanctum/User.php
@@ -1,24 +1,33 @@
 <?php
 
-namespace App\Models\Sanctum;
+namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
-class PersonalAccessToken extends SanctumPersonalAccessToken
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
     protected $fillable = [
-        'token',
-        'name'
+        'name',
+        'email',
+        'password',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
     protected $hidden = [
         'password',
         'remember_token',
